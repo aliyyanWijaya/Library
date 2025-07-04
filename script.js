74d363af3f2097fdfff72881b4830735f31f5ac0
aliyyanWijaya <muhammadqaliyyanwijaya@gmail.com>
2025-07-04 22:51:04 +1200

    create the skeleton and complete the function addBookToLibrary

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  }
+  this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary)
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
